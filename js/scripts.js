bd4a72ff6f453ef55168721561e08a3e7c8907b0
Megan Warnock and Nathaniel Meyer <nathaniel@>
2017-03-01 10:39:01 -0800

    Add support for requirements 3-5

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,8 @@ function rollDie() {
   max = Math.floor(6);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+var turnScore = 0;
+var totalScore = 0;
 // var echoFunction = function(string) {
 //   return string;
 // };
@@ -12,11 +13,22 @@ function rollDie() {
 // Front end logic
 $(function() {
   $("#rollDie").click(function(){
-    $(".response").show();
+    $("#holdOrRoll").show();
+    $("#hold").show();
     var currentRoll = rollDie();
+    turnScore += currentRoll;
     $("#rollDisplay").text(currentRoll);
+    $(".turnDisplay").text(turnScore);
   });
-  
+
+  $("#hold").click(function(){
+    $("#holdOrRoll").hide();
+    $("#endTurn").show();
+    totalScore += turnScore;
+    $(".turnDisplay").text(turnScore);
+    $(".totalDisplay").text(totalScore);
+  });
+
   // $("form#inputForm").submit(function(event){
   //   event.preventDefault();
     // userString = $("input#userInput").val();
