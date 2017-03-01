61fb25bbd4612d104b305c1bcaba228ed58b6453
Megan Warnock and Nathaniel Meyer <megan@>
2017-03-01 09:51:41 -0800

    Add support for first two business requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 // Business logic
-var echoFunction = function(string) {
-  return string;
-};
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// var echoFunction = function(string) {
+//   return string;
+// };
 
 // Front end logic
 $(function() {
   $("form#inputForm").submit(function(event){
     event.preventDefault();
-    userString = $("input#userInput").val();
-    if (!userString) {
-      $("#errorFeedback").show();
-    } else {
-      $("#errorFeedback").hide();
-      output = echoFunction(userString);
-      $(".response").show();
-      $("#echoInput").text(output);
-    }
-    echoInput = "";
+    // userString = $("input#userInput").val();
+    // if (!userString) {
+    //   $("#errorFeedback").show();
+    // } else {
+    //   $("#errorFeedback").hide();
+    //   output = echoFunction(userString);
+    $(".response").show();
+    //   $("#echoInput").text(output);
+    // }
+    debugger;
+    var currentRoll = getRandomIntInclusive(1, 6);
+    $("#rollDisplay").text(currentRoll);
   });
 });
