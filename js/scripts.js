01ff7f126cb790126de2185b9d66e9efed2f84e3
Megan Warnock and Nathaniel Meyer <nathaniel@>
2017-03-01 14:53:01 -0800

    Add a bunch of html styling and refucktor the back end

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@
 var currentRoll = 0;
 // var turnScore = 0;
 // var totalScore = 0;
+var player1 = new Player("Megan");
+var player2 = new Player("Nathaniel");
+this.player = player1;
+console.log(player1);
 
 function Player(name) {
   this.name = name;
@@ -24,39 +28,24 @@ Player.prototype.hold = function() {
   // something to switch players. here and maybe something else in the front end.
 }
 
-var endTurn = function() {
-  if (currentPlayer === player1) {
-    currentPlayer === player2;
-  }
-  else {
-    currentPlayer === player1;
-  }
-};
-
-// var echoFunction = function(string) {
-//   return string;
-// };
-
 // Front end logic
 $(function() {
   var hideAll = function() {
     $("#holdOrRoll").hide();
     $("#endTurnText").hide();
     $("#bust").hide();
-    $("#holdButton").hide();
+    $("#player1HoldButton").hide();
   };
-
+  $("#player2HoldButton").prop("disabled",true);
+  $("#player2RollDie").prop("disabled",true);
+  $("#player1TotalScore").text("0");
+  $("#player2TotalScore").text("0");
   hideAll();
 
-  var player1 = new Player("Megan");
-  console.log(player1);
-  // var player2 = new Player("Nathaniel");
-  // var currentPlayer = player1;
-
-  $("#rollDie").click(function(){
+  $("#player1RollDie").click(function(){
     hideAll();
     $("#holdOrRoll").show();
-    $("#holdButton").show();
+    $("#player1HoldButton").show();
     turnScore = player1.rollDie();
     console.log(player1);
     if (currentRoll === 1) {
@@ -67,33 +56,24 @@ $(function() {
     }
   });
 
-  $("#holdButton").click(function(){
+  $("#player1HoldButton").click(function(){
     hideAll();
     $("#endTurnText").show();
     totalScore = player1.hold();
     console.log(player1);
     $(".turnDisplay").text(this.turnScore);
     $(".totalDisplay").text(totalScore);
-    // endTurn();
+    endTurn();
   });
 
   var bust = function() {
     hideAll();
+
     $("#bust").show();
-    // endTurn();
+    endTurn();
   };
 
-  // Leaving this here for form syntax in case we later take players' names
-  // $("form#inputForm").submit(function(event){
-  //   event.preventDefault();
-    // userString = $("input#userInput").val();
-    // if (!userString) {
-    //   $("#errorFeedback").show();
-    // } else {
-    //   $("#errorFeedback").hide();
-    //   output = echoFunction(userString);
-    //   $("#echoInput").text(output);
-    // }
-    // debugger;
-  // });
+  var endTurn = function() {
+
+  };
 });
